48e8ba0d65f18f8821259056a67c19a7cce80807
chris <chrisnguyen367@gmail.com>
2026-09-08 19:44:03 -0500

    database get secrets from streamlit

--- database.py
+++ database.py
@@ -1,29 +1,24 @@
 from sqlalchemy import create_engine, Integer, String
 from sqlalchemy.pool import NullPool
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker
-from dotenv import load_dotenv
 import streamlit as st
 from urllib.parse import quote_plus
-import os
 
 
-
-load_dotenv()
-
-
-USER = os.getenv("user")
-RAW_PASSWORD = os.getenv("password")
-HOST = os.getenv("host")
-PORT = os.getenv("port")
-DBNAME = os.getenv("dbname")
+#sync this with streamlit secrets instead of os
+USER = st.secrets("user")
+RAW_PASSWORD = st.secrets("password")
+HOST = st.secrets("host")
+PORT = st.secrets("port")
+DBNAME = st.secrets("dbname")
 
 ENCODED_PASSWORD = quote_plus(RAW_PASSWORD)
 
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{ENCODED_PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
-@st.cache_resource
+@st.cache_resource(validate=False)
 def get_engine():
-    return create_engine(DATABASE_URL, poolclass=NullPool, pool_pre_ping=True, connect_args={"options" : "-c statment_timeout=5000"})
+    return create_engine(DATABASE_URL, poolclass=NullPool, pool_pre_ping=True, connect_args={"options" : "-c statement_timeout=5000"})
 
 engine = get_engine()
 
