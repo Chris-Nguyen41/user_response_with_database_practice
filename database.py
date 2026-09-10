ea0880fbced54ac00a7bc254ec1990e1f55697a3
chris <chrisnguyen367@gmail.com>
2026-09-10 01:12:24 -0500

    fixed error invalid response appearing enter response, session automattically commits and closes now

--- database.py
+++ database.py
@@ -5,31 +5,34 @@ import streamlit as st
 from urllib.parse import quote_plus
 
 
-
-USER = st.secrets["user"]
-RAW_PASSWORD = st.secrets["password"]
-HOST = st.secrets["host"]
-PORT = st.secrets["port"]
-DBNAME = st.secrets["dbname"]
+USER = st.secrets['user']
+RAW_PASSWORD = st.secrets['password']
+HOST = st.secrets['host']
+PORT = st.secrets['port']
+DBNAME = st.secrets['dbname']
 
 ENCODED_PASSWORD = quote_plus(RAW_PASSWORD)
 
 DATABASE_URL = f"postgresql+psycopg2://{USER}:{ENCODED_PASSWORD}@{HOST}:{PORT}/{DBNAME}?sslmode=require"
 
+
 @st.cache_resource(validate=False)
 def get_engine():
-    return create_engine(DATABASE_URL, poolclass=NullPool, pool_pre_ping=True, connect_args={"options" : "-c statement_timeout=5000"})
-
-engine = get_engine()
+    return create_engine(DATABASE_URL,
+                         poolclass=NullPool, 
+                         pool_pre_ping=True, 
+                         connect_args={"options" : "-c statement_timeout=5000"})
 
 class Base(DeclarativeBase):
     pass
 
 class User_Response(Base):
-    __tablename__ = "user_responses"
+    __tablename__ = 'user_responses'
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     response: Mapped[str] = mapped_column(String(3), nullable=False)
 
+engine = get_engine()
+
 Base.metadata.create_all(engine)
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+Session = sessionmaker(bind=engine, expire_on_commit=False)
